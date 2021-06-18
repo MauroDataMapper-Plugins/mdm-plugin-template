d0042ed31abcb5676309f56e7d4ea23c14f76202
Oliver Freeman <oliver.freeman@bdi.ox.ac.uk>
2021-06-18 14:42:02 +0100

    Update to mdmCore 4.6.0

--- src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/MdmPluginTemplateGrailsPlugin.groovy
+++ src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/MdmPluginTemplateGrailsPlugin.groovy
@@ -23,7 +23,7 @@ import grails.plugins.Plugin
 class MdmPluginTemplateGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "4.0.1 > *"
+    def grailsVersion = "4.0.6 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
